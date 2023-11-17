ada7041c794a2ce89245f57e413cbb42b4721d1d
TU2810 <PAT28102003@gmail.com>
2023-11-17 10:11:19 +0700

    slider 30

--- PHP/index.php
+++ PHP/index.php
@@ -7,17 +7,12 @@
 </head>
 <body>
     <?php 
-    $exchangeRate = array(22300, 27300, 17000, 120 );
-    $exchangeRate[] = 600 ;
-    $exchangeRate[] = 16000 ;
-    $exchangeRate[0] = 22200 ;
+    $exchangeRate = array("usd"=>22300,"eur"=> 27300,"sgd" => 17000,"jpy" => 120 );
     echo "<h3> Exchange rate vnd </h3>";
     echo "usd : $exchangeRate[0]<br>";
     echo "eur : $exchangeRate[1]<br>";
     echo "sgd : $exchangeRate[2]<br>";
     echo "jpy : $exchangeRate[3]<br>";
-    echo "chn : $exchangeRate[4]<br>";
-    echo "aud : $exchangeRate[5]<br>";
-    ?>
+ ?>
 </body>
 </html>
