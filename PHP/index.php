5f242921ea42e1c7c33b1d25f835bac4f7d31d89
TU2810 <PAT28102003@gmail.com>
2023-11-28 15:56:44 +0700

    slider 21

--- PHP/index.php
+++ PHP/index.php
@@ -1,16 +1,17 @@
 <?php 
-$items = array('item1.png' , 'item2.png' ,'item3.png','item4.png');
-foreach ($items as $item)
+function display_image_list($imageList)
 {
-  echo "<img src='images/>$item' width = 50em height = 50em alt= '$item' >";
+  foreach ($imageList as $item)
+  {
+    echo "< img src = 'images/$item' width = 50em height = 50em alt='$item'>";
 
+  }
 }
+$item = array ('item1.png','item2.png','item3.png','item4.png',);
+display_image_list($items);
 ?>
 <h1>function</h1>
 <?php 
 $items = array('item5.png' ,'item6.png');
-foreach ($items as $item)
-{
-  echo "< img src = 'images/$item' width = 50em height = 50em alt='$item'>";
-}
+display_image_list($items);
 ?>
