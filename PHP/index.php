232d28bb723636485db3b61daa124da682675c61
TU2810 <PAT28102003@gmail.com>
2023-11-28 15:51:34 +0700

    slider 20

--- PHP/index.php
+++ PHP/index.php
@@ -1,58 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Nhập ngày tháng năm</title>
-  <style>
-    body {
-      background-color: black;
-    }
-    .test {
-      width: 300px;
-      height: 100px;
-      background-color: #fff;
-      color: #333;
-      text-align: center;
-      border-radius: 5px;
-      margin:0 auto;
-    }
-    .get {
-      background-color: greenyellow;
-      border-radius: 5px;
-    }
-  </style>
-</head>
-<body>
-  <form class="test" action="result.php" method="get">
-    <h3>Chọn Ngày Tháng Năm </h3>
-<label for=""> Ngay:</label>
-<select name="day" id="day">
-  <?php
-  for($day = 1 ; $day <=31 ; $day++)
-  echo"<option value=\"$day\">$day</option>";
-  ?>
-</select>
+<?php 
+$items = array('item1.png' , 'item2.png' ,'item3.png','item4.png');
+foreach ($items as $item)
+{
+  echo "<img src='images/>$item' width = 50em height = 50em alt= '$item' >";
 
-<label for=""> Thang:</label>
-<select name="month" id="month">
-  <?php
-  for($month = 1 ; $month <=31 ; $month++)
-  echo"<option value=\"$month\">$month</option>";
-  ?>
-</select>
-
-<label for=""> Nam:</label>
-<select name="year" id="year">
-  <?php
-  //lay nam hien tai
-  $today = getdate();
-  $curYear = $today["year"];
-  for($year = 1900 ; $year <= $curYear ; $year++)
-  echo"<option value=\"$year\">$year</option>";
-  ?>
-</select><br><br>
-<input class="get" type="submit" value="chọn">
-  </form>
-</body>
-</html>
+}
+?>
+<h1>function</h1>
+<?php 
+$items = array('item5.png' ,'item6.png');
+foreach ($items as $item)
+{
+  echo "< img src = 'images/$item' width = 50em height = 50em alt='$item'>";
+}
+?>
